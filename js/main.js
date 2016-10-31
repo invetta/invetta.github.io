e9c0e04405f5d313dc2ab2ba361ef4f3884f3811
sNiCKY <mpruszynski@gmail.com>
2016-10-31 19:06:39 +0100

    working version

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function($) {
+  $(document).ready(function() {
+    var $offerSlider = $('#i-offer-slider');
+    var $offertSliderDots = $('#i-offer-slider-dots');
+
+    // Slick.js documentation: http://kenwheeler.github.io/slick
+    $offerSlider.slick({
+      dots: true,
+      appendDots: $offertSliderDots,
+      arrows: false,
+      adaptiveHeight: true
+    });
+
+    window.goToSlide = function(n) {
+      $offerSlider.slick('slickGoTo', n);
+    }
+
+    $('#i-about-us-slider').slick();
+  });
+}(jQuery));
